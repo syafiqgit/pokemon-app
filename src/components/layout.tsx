9179cee73fd9a8cb4fa34ee834b3d01652f7c9b2
Syafiq <syafiqsteam744@gmail.com>
2024-01-25 20:05:44 +0700

    feat: change background

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Layout(props: Props) {
   const { children } = props;
   return (
-    <div className="w-full h-screen flex justify-center overflow-auto">
+    <div className="w-full h-screen flex justify-center overflow-auto bg-slate-500">
       <div className="w-full md:max-w-screen-md flex flex-col">
         <div className="grow overflow-auto">{children}</div>
         <Navbar/>
